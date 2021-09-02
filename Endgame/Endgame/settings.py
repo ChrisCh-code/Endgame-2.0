274c234082a945835e19d46dd84df38e34b8695f
Chris Chau <58018482+ChrisCh-code@users.noreply.github.com>
2021-09-02 16:42:57 -0400

    Update settings.py

--- Endgame/Endgame/settings.py
+++ Endgame/Endgame/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3wsh-w8k5=$!*abc^_wk6e9u^=ago_30lgu8lq)ja(*m$myw*#'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
