581fa07d6b8ac7f001834ee13ba8d87dfef39c08
Chris Chau <chauchris01@gmail.com>
2021-09-17 17:38:54 -0400

    adding static file, templates, and Boostrap

--- Endgame/blog/views.py
+++ Endgame/blog/views.py
@@ -2,9 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+posts = [
+
+    {
+        'author': 'Mike Tyson',
+        'title': 'post me',
+        'content': 'first post',
+        'date_posted': 'january 2nd 2022'
+    },
+    {
+        'author': 'Huy Chau',
+        'title': 'post me',
+        'content': 'second post',
+        'date_posted': 'march 12, 2022'
+    }
+
+]
+
 
 def home(request):
-    return HttpResponse('<h1>blog home</h1>')
+    context = {
+        'posts': posts 
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>blog about</h1>')
+    return render(request, 'blog/about.html')
