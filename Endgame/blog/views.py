58ebb612858db16ee6bc4e5bfafe3223fd920b7f
Chris Chau <chauchris01@gmail.com>
2021-09-18 16:02:27 -0400

    added Post model

--- Endgame/blog/views.py
+++ Endgame/blog/views.py
@@ -1,28 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
-posts = [
 
-    {
-        'author': 'Mike Tyson',
-        'title': 'post me',
-        'content': 'first post',
-        'date_posted': 'january 2nd 2022'
-    },
-    {
-        'author': 'Huy Chau',
-        'title': 'post me',
-        'content': 'second post',
-        'date_posted': 'march 12, 2022'
-    }
-
-]
 
 
 def home(request):
     context = {
-        'posts': posts 
+        'posts': Post.objects.all() 
     }
     return render(request, 'blog/home.html', context)
 
