e0b1dbbd5d45385425503f558a10ec439ca6bfd2
Chris Chau <chauchris01@gmail.com>
2021-09-23 15:43:39 -0400

    added resizing to ImageField and profile pics

--- Endgame/blog/models.py
+++ Endgame/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django_resized import ResizedImageField
 
 # Create your models here.
 
@@ -9,7 +10,8 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    img = models.ImageField(null=True,blank=True, upload_to='media/')
-
+    #img = models.ImageField(null=True,blank=True, upload_to='media/')
+    img = ResizedImageField(size=[640,480], upload_to='media/', blank=True, null=True,quality=100)
+    
     def __str__(self):
         return self.title
