c7e254111dcf3fc93072f108912e973e9fc88cf9
Chris Chau <chauchris01@gmail.com>
2021-09-24 14:32:17 -0400

    added class-based ListView

--- Endgame/blog/views.py
+++ Endgame/blog/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView
 
 # Create your views here.
 
 
 
-def home(request):
-    context = {
-        'posts': Post.objects.all() 
-    }
-    return render(request, 'blog/home.html', context)
+# def home(request):
+#     context = {
+#         'posts': Post.objects.all() 
+#     }
+#     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' 
+    context_object_name = 'posts' # set variable for template to loop through
+    ordering = ['date_posted']
+    
 
 def about(request):
     return render(request, 'blog/about.html')
