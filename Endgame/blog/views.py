05e4f31d939841741eaeeaf3c3019f22065ac4d9
Chris Chau <chauchris01@gmail.com>
2021-09-28 16:05:26 -0400

    added UserPostListView

--- Endgame/blog/views.py
+++ Endgame/blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 from django.views.generic import ListView, DetailView,CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -13,6 +14,16 @@ class PostListView(ListView):
     context_object_name = 'posts' # set variable for template to loop through
     ordering = ['-date_posted']
     paginate_by = 5
+    
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html' 
+    context_object_name = 'posts' # set variable for template to loop through
+    paginate_by = 5
+    
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
     
 class PostDetailView(DetailView):
     model = Post
