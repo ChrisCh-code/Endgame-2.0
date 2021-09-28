88bab748c631ae9729867058df3f399d54ba2ae3
Chris Chau <chauchris01@gmail.com>
2021-09-28 15:14:37 -0400

    added CRUD views for post

--- Endgame/blog/models.py
+++ Endgame/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django_resized import ResizedImageField
+from django.urls import reverse
 
 # Create your models here.
 
@@ -15,3 +16,7 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+        
+        
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
